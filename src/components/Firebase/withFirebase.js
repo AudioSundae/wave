f1a6e3a77faed6dd39f401bb0a6cfa4e65b25818
mkochakian <mattk4@me.com>
2019-06-23 12:11:55 -0400

    api connected

--- src/components/Firebase/withFirebase.js
+++ src/components/Firebase/withFirebase.js
@@ -1,59 +1,40 @@
 import firebase from './firebase';
+import provider from './provider';
 import React from 'react';
 import Spinner from '../Widgets/spinner.js'
-
-var provider = new firebase.auth.GoogleAuthProvider();
-provider.addScope('profile');
-provider.addScope('email');
-provider.addScope('https://www.googleapis.com/auth/gmail.readonly')
-
-
-
+let gapi = window.gapi;
 
 const withFirebase = Component => {
   class WithFirebase extends React.Component {
     state = { user: false }
+    async login() {
+      const googleAuth = gapi.auth2.getAuthInstance(),
+            googleUser = await googleAuth.signIn(),
+            token = googleUser.getAuthResponse().id_token,
+            credential = provider.credential(token);
+      await firebase.auth().signInWithCredential(credential);
+    }
     componentDidMount() {
+      this.setState({hello: true})
+      gapi.load("client:auth2", () => {
+        console.log('loaded client')
+        gapi.client.init({
+          apiKey: "AIzaSyCwmdmI3DVrOD-Tdromu8N-J-gXWqvcBe4",
+          clientId: "795253427550-e7lvvtbellcidlur1p2kjvesle8tt7vl.apps.googleusercontent.com",
+          discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest"],
+          scope: "https://www.googleapis.com/auth/gmail.readonly"
+        })
+        // this.login()
+        .catch(error => console.log(error))
+      })
       firebase.auth().onAuthStateChanged(user => {
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = 'https://apis.google.com/js/api.js';
-        script.onload = () => {
-          window.gapi.load('client:auth2', () => {
-            window.gapi.client.init({
-               apiKey: "AIzaSyCwmdmI3DVrOD-Tdromu8N-J-gXWqvcBe4",
-               clientId: "795253427550-e7lvvtbellcidlur1p2kjvesle8tt7vl.apps.googleusercontent.com",
-               discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest"],
-               scope: "https://www.googleapis.com/auth/gmail.readonly",
-            })
-            .then(() => {
-              var googleUser = window.gapi.auth2.getAuthInstance().currentUser.get()
-              var isSignedIn = window.gapi.auth2.getAuthInstance().isSignedIn.get()
-              if (isSignedIn) {
-                 // get the credentials from the google auth response
-                 var idToken = googleUser.getAuthResponse().id_token;
-                 var creds = firebase.auth.GoogleAuthProvider.credential(idToken);
-                 // auth in the user
-                 firebase.auth().signInWithCredential(creds).then((user) => {
-                    // you can use (user) or googleProfile to setup the user
-                    var googleProfile = googleUser.getBasicProfile()
-                    console.log(1, user)
-                    console.log(2, googleUser)
-                    if (user) {
-                      // return this.setState({
-                      //   user: result.user,
-                      //   token: result.credential.accessToken
-                      // })
-                    } else {
-                      firebase.auth().signOut().then(() => firebase.auth().signInWithRedirect(provider))
-                    }
-                 })
-              }
-            })
+        if (user) {
+          this.setState({
+            user
           })
+        } else {
+          firebase.auth().signOut().then(() => this.login())
         }
-        // bind this to your single page app...
-        document.getElementsByTagName('head')[0].appendChild(script);
       })
     }
     render() {
--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import Header from '../Header';
+// import Header from '../Header';
 import firebase from '../Firebase/firebase';
 
 export default class extends React.Component {
+  componentDidMount() {
+    window.gapi.client.load("gmail", "v1", () => console.log("gmail loaded"))
+  }
   render() {
     return(
-      <div className="notifications-module col">
-        <Header />
+      <div className="notifications-module col-c-c">
+        <span>{(!!this.props.user).toString()}</span>
         <button onClick={() => firebase.doSignOut()}>sign out</button>
       </div>
     )
